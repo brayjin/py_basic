08916f46a228a8353463e5733437a0d068ed7f03
brayjin <brayjin01@gmail.com>
2025-03-14 19:27:36 +0530

    enchanced readability

--- unique.py
+++ unique.py
@@ -1,14 +1,23 @@
 import re
 
-def rm_dupi(str_input: list) -> list:
+def remove_dupilicate(str_input: list) -> list:
+    """Removes duplicate strings from a list and returns a sorted list of unique strings."""
     return sorted(set(str_input))
 
 def main():
-    in_str = input("Enter the list of words: ")
-    in_str = re.sub(r'[^a-zA-Z0-9 ]', "",in_str)
-    dp_list = in_str.split()
-    print(f"Given list: {dp_list}")
-    uniq = rm_dupi(dp_list)
-    print(f"New list: {uniq}")
+    """Gets user input, removes non-alphanumeric characters, splits into words, removes duplicates, and prints the results."""   
+    input_str = input("Enter the list of words (seperated by spaces): ")
+    
+    # Remove non-alphanumerical characters and extra spaces
+    cleaned_input = re.sub(r'[^a-zA-Z0-9 ]', "",input_str)
 
-main()
+    duplicate_list = cleaned_input.split()
+    
+    print(f"Given list: {duplicate_list}")
+    
+    unique_list = remove_dupilicate(duplicate_list)
+    
+    print(f"New list: {unique_list}")
+
+if __name__ == "__main__":
+    main()
